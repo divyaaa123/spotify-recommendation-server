6d5e4673085b93372a17fa8dc8a3028c212decf2
divyaaa123 <divya42jain@gmail.com>
2024-11-10 21:38:18 +0530

    add barebone structure

---
+++ src/main/java/com/panda/spotify_recommendation_server/SpotifyRecommendationServerApplication.java
@@ -0,0 +1,13 @@
+package com.panda.spotify_recommendation_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyRecommendationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyRecommendationServerApplication.class, args);
+	}
+
+}
